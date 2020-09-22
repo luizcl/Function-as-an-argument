fe3c06951bc2103b7f87ecd732967b2ea825cf75
Luiz <luiz.cl@live.com>
2020-09-22 15:04:35 -0300

    Regular code, old version

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
 
 import entities.Product;
+import model.services.ProductService;
 
 public class Program {
 
@@ -17,9 +17,14 @@ public class Program {
 		
 		list.add(new Product("TV", 900.0));
 		list.add(new Product("Mouse", 50.0));
-		list.add(new Product("Tablet", 250.50));
+		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
+		ProductService ps = new ProductService();
+		
+		double sum = ps.filteredSum(list);
+		
+		System.out.println("Sum= " + String.format("%.2f", sum));
 				
 	}
 	
